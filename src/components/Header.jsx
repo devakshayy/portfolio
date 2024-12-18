da86ed8cbfe15b64b358ae687c8bc9608ee4d5d5
devakshayy <devakshay916@gmail.com>
2024-12-19 01:32:21 +0530

    the 3 pages completed

---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+const Header = () => {
+  return (
+     <header className="grid grid-cols-1 lg:grid-cols-2">
+        <div className="sm:py-4 py-3 lg:px-20 md:px-15 flex justify-center items-center">
+          <img src="/profile-pic.png" alt="profile-pic" className="h-[200px] sm:h-[500px]" />
+        </div>
+        <div className="sm:p-10 flex justify-center items-center flex-col gap-2 sm:gap-5">
+          <div className="sm:text-3xl text-xl font-bold text-gray-700">Hi I'm</div>
+          <div className="sm:text-6xl text-2xl font-bold text-gray-900">Akshay,</div>
+          <div className="sm:text-3xl text-xl font-bold text-gray-700">
+            a Frontend Developer
+          </div>
+          <div className="flex gap-2 sm:gap-3">
+            <button
+              className="ring-1 p-2 sm:p-3 rounded-full px-2 sm:px-6 hover:bg-gray-800
+                                    hover:text-white transtition duration-300"
+            >
+              Download CV
+            </button>
+            <button
+              className="bg-gray-700 text-white hover:bg-gray-900
+                                  px-3 sm:px-6 p-2 sm:p-3 rounded-full transition transform"
+            >
+              Contact Info
+            </button>
+          </div>
+          <div className="flex gap-2 mt-1">
+            <img src="/github.png" alt="github" className="h-7 sm:h-10" />
+            <img src="/linkedin.png" alt="linkedin" className="h-7 sm:h-10" />
+          </div>
+        </div>
+      </header>
+  )
+}
+
+export default Header
---
+++ src/pages/Experience.jsx
@@ -0,0 +1,113 @@
+const Experience = () => {
+  return (
+    <div className="experience text-center p-5 lg:p-10 gap-5 lg:gap-10 " id="experience">
+    <div className="text-gray-500 text-lg">Explore My</div>
+    <div className="text-gray-900  text-3xl sm:text-5xl font-bold">Experience</div>
+    <div className="grid grid-cols-1 lg:grid-cols-2 space-y-10 lg:space-y-0 lg:space-x-10 py-10">
+      <div className="ring-1 p-1 py-4 sm:p-10 sm:py-18 rounded-3xl  ">
+        <div className="text-gray-900 text-2xl sm:text-3xl mb-4 font-bold">Frontend Development</div>
+        <div className="grid grid-cols-2 lg:grid-cols-3 justify-items-center lg:gap-2 items-center"> 
+
+          <div className="flex justify-between  items-center gap-3 p-5">
+            <div className="flex flex-col justify-center items-center">
+            <img src="/checkmark.png" className="h-10" />
+              <div className="text-gray-900 text-md font-bold">HTML</div>
+              <div className="text-gray-600 text-sm sm:text-md font-semibold">Intermediate</div>
+            </div>
+          </div>
+          <div className="flex justify-between  items-center gap-3 p-5">
+            <div className="flex flex-col justify-center items-center">
+            <img src="/checkmark.png" className="h-10" />
+              <div className="text-gray-900 text-md font-bold">CSS</div>
+              <div className="text-gray-600 text-sm sm:text-md  font-semibold">Intermediate</div>
+            </div>
+          </div>
+          <div className="flex justify-between  items-center gap-3 p-5">
+            <div className="flex flex-col justify-center items-center">
+            <img src="/checkmark.png" className="h-10" />
+              <div className="text-gray-900 text-md font-bold">JAVASCRIPT</div>
+              <div className="text-gray-600 text-sm sm:text-md font-semibold">Intermediate</div>
+            </div>
+          </div>
+          <div className="flex justify-between  items-center gap-3 p-5">
+            <div className="flex flex-col justify-center items-center">
+            <img src="/checkmark.png" className="h-10" />
+              <div className="text-gray-900 text-md font-bold">REACT JS</div>
+              <div className="text-gray-600 text-sm sm:text-md  font-semibold">Intermediate</div>
+            </div>
+          </div>
+          <div className="flex justify-between  items-center gap-3 p-5">
+            <div className="flex flex-col justify-center items-center">
+            <img src="/checkmark.png" className="h-10" />
+              <div className="text-gray-900 text-md font-bold">NEXT JS</div>
+              <div className="text-gray-600 text-sm sm:text-md font-semibold">Intermediate</div>
+            </div>
+          </div>
+          <div className="flex justify-between  items-center gap-3 p-5">
+            <div className="flex flex-col justify-center items-center">
+            <img src="/checkmark.png" className="h-10" />
+              <div className="text-gray-900 text-md font-bold">WORDPRESS</div>
+              <div className="text-gray-600 text-sm sm:text-md  font-semibold">Intermediate</div>
+            </div>
+          </div>
+        
+        </div>
+
+      </div>
+      <div className="ring-1 p-1 py-4 sm:p-10 sm:py-18 rounded-3xl  ">
+        <div className="text-gray-900 text-2xl sm:text-3xl mb-4 font-bold">Backend Development</div>
+        <div className="grid grid-cols-2 lg:grid-cols-3 justify-items-center lg:gap-2 items-center"> 
+
+          <div className="flex justify-between  items-center gap-3 p-5">
+            <div className="flex flex-col justify-center items-center">
+            <img src="/checkmark.png" className="h-10" />
+              <div className="text-gray-900 text-md font-bold">REACT JS</div>
+              <div className="text-gray-600 text-sm sm:text-md font-semibold">Intermediate</div>
+            </div>
+          </div>
+          <div className="flex justify-between  items-center gap-3 p-5">
+            <div className="flex flex-col justify-center items-center">
+            <img src="/checkmark.png" className="h-10" />
+              <div className="text-gray-900 text-md font-bold">JSON</div>
+              <div className="text-gray-600 text-sm sm:text-md  font-semibold">Intermediate</div>
+            </div>
+          </div>
+          <div className="flex justify-between  items-center gap-3 p-5">
+            <div className="flex flex-col justify-center items-center">
+            <img src="/checkmark.png" className="h-10" />
+              <div className="text-gray-900 text-md font-bold">SQL</div>
+              <div className="text-gray-600 text-sm sm:text-md font-semibold">Intermediate</div>
+            </div>
+          </div>
+          <div className="flex justify-between  items-center gap-3 p-5">
+            <div className="flex flex-col justify-center items-center">
+            <img src="/checkmark.png" className="h-10" />
+              <div className="text-gray-900 text-md font-bold">LARAVEL</div>
+              <div className="text-gray-600 text-sm sm:text-md  font-semibold">Intermediate</div>
+            </div>
+          </div>
+          <div className="flex justify-between  items-center gap-3 p-5">
+            <div className="flex flex-col justify-center items-center">
+            <img src="/checkmark.png" className="h-10" />
+              <div className="text-gray-900 text-md font-bold">MONGO DB</div>
+              <div className="text-gray-600 text-sm sm:text-md font-semibold">Intermediate</div>
+            </div>
+          </div>
+          <div className="flex justify-between  items-center gap-3 p-5">
+            <div className="flex flex-col justify-center items-center">
+            <img src="/checkmark.png" className="h-10" />
+              <div className="text-gray-900 text-md font-bold">EXPRESS JS</div>
+              <div className="text-gray-600 text-sm sm:text-md  font-semibold">Intermediate</div>
+            </div>
+          </div>
+        
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+  )
+}
+
+export default Experience
