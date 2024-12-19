7cc0d829cfcb6e0cb1da5cab75fd392604b23cdd
devakshayy <devakshay916@gmail.com>
2024-12-19 18:05:32 +0530

    contact page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import About from "./pages/About";
+import Contact from "./pages/Contact";
 import Experience from "./pages/Experience";
 import Projects from "./pages/Projects";
 
@@ -12,41 +13,9 @@ function App() {
        <About/>
        <Experience/>
        <Projects/>
+       <Contact/>
      </>
   );
 }
 
 export default App;
-
-
-//       {/* Projects */}
-
-//       {/* Contact Us */}
-
-//       <div className="about p-32 pb-0 text-center">
-//         <div className="text-gray-500 text-lg">to</div>
-//         <div className="text-gray-900 text-5xl font-bold">Contact</div>
-//         <div className="flex justify-center items-center p-10">
-//           <div className="ring-1 rounded-3xl p-10 flex gap-5 bg-gray-100">
-//             <div className="flex">
-//               <img src="/email.png" alt="email" className="h-10" />
-//               <div className="text-3xl text-semi-bold hover:text-red-700 transition">
-//                 example@gmail.com
-//               </div>
-//             </div>
-//             <div className="flex">
-//               <img src="/email.png" alt="email" className="h-10" />
-//               <div className="text-3xl text-semi-bold hover:text-red-700 transition">
-//                 linkedin.com
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//         <div className="text-xl text-gray-600 p-10 pb-20">
-//           Copyright <b>©</b> {new Date().getFullYear()} Akshay. All Rights
-//           Reserved
-//         </div>
-//       </div>
-
-//       {/* Contact Us */}
-// </>
